ddda684c874380436a69ea6a7879e4a36463d5d9
Marco-Roa <31865963+Marco-Roa@users.noreply.github.com>
2017-11-08 18:25:30 -0600

    Initial Commit

---
+++ StarWarsAPI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StarWarsAPI
+//
+//  Created by Marco Antonio Roa De La Cruz on 11/8/17.
+//  Copyright © 2017 MarcoRoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StarWarsAPI/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StarWarsAPI
+//
+//  Created by Marco Antonio Roa De La Cruz on 11/8/17.
+//  Copyright © 2017 MarcoRoa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
